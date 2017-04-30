b0333b074f756c04e010d5dc0a587e7d044ad5f9
Richarbe <benr239@gmail.com>
2017-04-30 16:24:16 -0700

    Working density and symmetry w/ output

--- HW4.py
+++ HW4.py
@@ -11,34 +11,43 @@ class Numimg:
         self.rows = len(self.Array)
         self.cols = len(self.Array[0])
         self.density = self.density()
-        self.h_symetry = self.__h_symmetry__()
-        self.v_symetry = self.__v_symmetry__()
-        self.min_h_intercepts, self.max_h_intercepts = self.__h_intercepts__()
-        self.min_v_intercepts, self.max_v_intercepts = self.__v_intercepts__()
+        self.h_symmetry = self.__h_symmetry__()
+        self.v_symmetry = self.__v_symmetry__()
+        #self.min_h_intercepts, self.max_h_intercepts = self.__h_intercepts__()
+        #self.min_v_intercepts, self.max_v_intercepts = self.__v_intercepts__()
 
     def print(self):
-        print(self.label, ":")
+        print("Label: ", self.label)
+        print("density: ", self.density)
+        print("h_symmetry: ", self.h_symmetry)
+        print("v_symmetry: ", self.v_symmetry)
         for i in self.Array:
             print(i)
+
     def density(self):
         counter = 0
-        total = 0
         for i in self.Array:
             for j in i:
                 if(j == '1'):
                     counter += 1
-                total += 1
 
         return counter/(self.rows * self.cols)
 
     def __h_symmetry__(self):
         temp = 0
-        for i in range(self.rows):
-            for j in range(self.cols):
-                if(self.Array[i][j] != self.Array[i][self.cols-(i+1)]):
+        for i in range(self.rows//2):
+            for j in range(self.cols//2):
+                if(self.Array[i][j] != self.Array[i][self.cols-(j+1)]):
                     temp += 1
         return temp
 
+    def __v_symmetry__(self):
+        temp = 0
+        for i in range(self.rows//2):
+            for j in range(self.cols//2):
+                if(self.Array[i][j] != self.Array[self.rows-(i+1)][j]):
+                    temp += 1
+        return temp
 
 
 
@@ -56,7 +65,15 @@ def parseData():
     for i in dataarray:
         Data.append(Numimg(i)) #you know it
 
+def printData():
+    global Data
+    for i in Data:
+        i.print()
 
+def main():
+    parseData()
+    printData()
+    return 0
 
 
 if __name__ == "__main__":
