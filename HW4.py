943d6e03a79cb8e006cfdcb2f9776b08f4e9a4b3
Richarbe <benr239@gmail.com>
2017-04-30 15:53:30 -0700

    Stuff
    not working, but started Numbing class

--- HW4.py
+++ HW4.py
@@ -1,16 +1,41 @@
 
+Data = []
+
+class Numimg:
+    def __init__(self, data = ""):
+        self.label = data[0]
+        temp = data[1:].split("\n") #like firewood
+        self.Array = []
+        for i in temp:
+            self.Array.append(i.split(" ")[1:]) #because
+        self.density = self.density()
+        self.h_symetry = self.__h_symetry__()
+        self.v_symetry = self.__v_symetry__()
+        self.min_h_intercepts, self.max_h_intercepts =__h_intercepts__()
+        self.min_v_intercepts, self.max_v_intercepts =__v_intercepts__()
+    def print(self):
+        print(self.label, ":")
+        for i in self.Array:
+            print(i)
 
 
-def main():
-    I = float(0)
-    II = float(0)
-    III = float(0)
-    IV = float(0)
-    f = open('testdata', 'r')
 
-    while(f.readline()):
-        for i in range(28):
-            print(f.readline())
+
+def parseData():
+    global Data
+    global cols, rows
+
+    print("Opening Test Data File")
+    f = open('Smalltest.txt', 'r')
+    print("Reading File to String")
+    tempdata = f.read()
+    print("Splitting into Samples")
+    dataarray = tempdata.split("\n\n\n") #yep
+    dataarray.pop()
+    for i in dataarray:
+        Data.append(Numimg(i)) #you know it
+
+
 
 
 if __name__ == "__main__":
